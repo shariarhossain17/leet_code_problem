a758197a9ce9dc00f72dbc23fd755ac448716284
shariar17 <shariarhossain23@gmail.com>
2026-01-15 21:40:21 +0600

    solved

---
+++ palindrome_number.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x;
+    cin >> x;
+    if (x < 0)
+        return 0;
+    int copy = x;
+    unsigned long long int y = 0;
+    while (copy)
+    {
+        y *= 10;
+        y += (copy % 10);
+        copy /= 10;
+    }
+    bool state = x == y ? 1 : 0;
+
+    return state;
+
+    return 0;
+}
