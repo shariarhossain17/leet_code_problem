c62b848c82cb32fc9ad8b2cd2da96e1a793dc31d
shariar17 <shariarhossain23@gmail.com>
2026-01-23 16:26:38 +0600

    solved andother way

--- remove_adjacent_duplicate.cpp
+++ remove_adjacent_duplicate.cpp
@@ -27,6 +27,36 @@ public:
 
         cout << ans << endl;
     }
+
+    void remove_duplicate_brute_force(string &s)
+    {
+
+        int n = s.size();
+
+        stack<char> st;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (st.empty() || st.top() != s[i])
+                st.push(s[i]);
+            else
+            {
+                st.pop();
+            }
+        }
+
+        string ans = "";
+
+        while (!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        cout << ans << endl;
+    }
 };
 int main()
 {
@@ -36,6 +66,7 @@ int main()
     cin >> s;
 
     Solution Sol;
-    Sol.remove_duplicate_better(s);
+    // Sol.remove_duplicate_better(s);
+    Sol.remove_duplicate_brute_force(s);
     return 0;
 }
